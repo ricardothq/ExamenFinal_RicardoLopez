3355a048cbaf52756fde554bc60998d56be21866
36542310Aa <ricardothq@gmail.com>
2021-10-29 09:55:03 -0400

    examen rick

---
+++ EjercicioArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EjercicioArray
+{
+    class Program
+    {
+        static void Main(string[] args, int multiplos)
+        {
+            int mul;
+		    int tamArreglo = 0;
+		    int num = 0;
+
+            Console.WriteLine("Tamaño del arreglo: ");
+		    tamArreglo=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese un nuemero para hallar sus multiplos ");
+		    num=int.Parse(Console.ReadLine());
+
+		    for(int i =0; i < tamArreglo; i++)
+            {
+                mul = (i+1)*num;
+		    }
+
+    }
+}
